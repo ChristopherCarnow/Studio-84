087b1d2e7b1105d6d2ddebf8edcc0761b69b2683
ChristopherCarnow <49200685+ChristopherCarnow@users.noreply.github.com>
2022-10-03 21:45:07 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,3 +2,48 @@
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+// Close mobile navigation:
+const alllinks = document.querySelectorAll("a:link"); // a = anchors  :link = anchors with href only
+
+alllinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null, //where it starts
+    threshold: 0, //how much is visible. could be 1 (completely in view)
+    rootMargin: "-80px", //height of navigation bar. must be px
+  }
+);
+obs.observe(sectionHeroEl);
